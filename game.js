62e4876a79e582e68f67c69749c430bca995a1f8
hwl9 <hwl9@sfu.ca>
2019-02-23 13:37:59 -0800

    Set initial position

--- game.js
+++ game.js
@@ -4,72 +4,54 @@ if(!PIXI.utils.isWebGLSupported()){
     type = "canvas"
 }
 
-var stage = new PIXI.Container();
-var renderer = PIXI.autoDetectRenderer(256, 256);
-document.body.appendChild(renderer.view);
+// var stage = new PIXI.Container();
+// var renderer = PIXI.autoDetectRenderer(256, 256);
+// document.body.appendChild(renderer.view);
 
-PIXI.loader.add("images/cat.png").load(setup);
+// PIXI.loader.add("images/cat.png").load(setup);
 
+// function setup() {
+//   var cat = new PIXI.Sprite(PIXI.loader.resources["images/cat.png"].texture);
+
+//   stage.addChild(cat);
+
+//   renderer.render(stage);
+// }
+
+//Create a Pixi Application
+let app = new PIXI.Application({ 
+    width: 800,         // default: 800
+    height: 600,        // default: 600
+    antialias: true,    // default: false
+    transparent: false, // default: false
+    resolution: 1       // default: 1
+    }
+);
+
+const mainCharacter = "images/mainCharacter.jpg";
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+//load an image and run the `setup` function when it's done
+PIXI.loader
+  .add([
+    mainCharacter
+  ])
+  .load(setup);
+
+
+
+
+//This `setup` function will run when the image has loaded
 function setup() {
-  var cat = new PIXI.Sprite(PIXI.loader.resources["images/cat.png"].texture);
 
-  stage.addChild(cat);
+  // Create the cat sprite
+  let player = new PIXI.Sprite(PIXI.loader.resources[mainCharacter].texture);
 
-  renderer.render(stage);
-}
-
-// //Create a Pixi Application
-// let app = new PIXI.Application({ 
-//     width: 256,         // default: 800
-//     height: 256,        // default: 600
-//     antialias: true,    // default: false
-//     transparent: false, // default: false
-//     resolution: 1       // default: 1
-//     }
-// );
-
-
-// //Add the canvas that Pixi automatically created for you to the HTML document
-// document.body.appendChild(app.view);
-
-// //load an image and run the `setup` function when it's done
-// PIXI.loader
-//   .add("images/cat.png")
-//   .load(setup);
-
-// //This `setup` function will run when the image has loaded
-// function setup() {
-
-//   //Create the cat sprite
-//   let cat = new PIXI.Sprite(PIXI.loader.resources["images/cat.png"].texture);
+  // Initial Position
+  player.position.set(400, 400);
   
-//   //Add the cat to the stage
-//   app.stage.addChild(cat);
-// }
-
-
-// //Add the canvas that Pixi automatically created for you to the HTML document
-// document.body.appendChild(app.view);
-
-// // PIXI.utils.sayHello(type);
-
-// // Create a new sprite using the texture
-// // let skier = "images/mainCharacter.jpg";
-// let skier = "images/cat.png";
-
-// // let texture = PIXI.utils.TextureCache[skier];
-// // let sprite = new PIXI.Sprite(texture);
-
-// // Load the skier
-// PIXI.loader
-//   .add(skier)
-//   .load(setup);
-
-// // Create a skier sprite
-// function setup() {
-//   let sprite = new PIXI.Sprite(
-//     PIXI.loader.resources[skier].texture
-//   );
-
-//   app.stage.addChild(sprite);
-// }
+  //Add the cat to the stage
+  app.stage.addChild(player);
+}
