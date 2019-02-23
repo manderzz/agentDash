997ade7e8acf047583be80b88831be9a49082964
hwl9 <hwl9@sfu.ca>
2019-02-23 15:17:26 -0800

    Center player; background change

--- game.js
+++ game.js
@@ -20,7 +20,7 @@ if(!PIXI.utils.isWebGLSupported()){
 
 //Create a Pixi Application
 let app = new PIXI.Application({ 
-    width: 900,         // default: 800
+    width: 1200,         // default: 800
     height: 800,        // default: 600
     antialias: true,    // default: false
     transparent: false, // default: false
@@ -38,40 +38,37 @@ PIXI.loader
   .add([mainCharacter])
   .load(setup);
 
+app.renderer.backgroundColor = 0xfcfcf9;
 
-let player
 
 //This `setup` function will run when the image has loaded
 function setup() {
-	//Create the `tileset` sprite from the texture
+    //Create the `tileset` sprite from the texture
     // let texture = PIXI.utils.TextureCache[mainCharacter];
     let texture = PIXI.BaseTexture.fromImage(mainCharacter);
 
-  // // Create the cat sprite
-  // let player = new PIXI.Sprite(PIXI.loader.resources[mainCharacter].texture);
+    //Create a rectangle object that defines the position and
+    //size of the sub-image you want to extract from the texture
+    //(`Rectangle` is an alias for `PIXI.Rectangle`)
 
-  //Create a rectangle object that defines the position and
-  //size of the sub-image you want to extract from the texture
-  //(`Rectangle` is an alias for `PIXI.Rectangle`)
+    // Array of textures for player
+    let textures = [];
 
-  let textures = [];
-
-  for (var i=0;i<3;i++) {
-    for (var j=0;j<2;j++) {
-      let rectangle = new PIXI.Rectangle(i*128,j*128,128,128);
-      let smallTexture = new PIXI.Texture(texture, rectangle);
-
-      textures.push(smallTexture);
+    for (var i=0;i<3;i++) {
+        for (var j=0;j<2;j++) {
+            let rectangle = new PIXI.Rectangle(i*128,j*128,128,128);
+            let smallTexture = new PIXI.Texture(texture, rectangle);
+            textures.push(smallTexture); // Append texture to the array of textures
+        }
     }
-  }
 
-  let defaultTexture = textures[2];
+    let defaultTexture = textures[2];
 
-  let player = new PIXI.Sprite(defaultTexture);
+    let player = new PIXI.Sprite(defaultTexture);
 
-  player.position.set(64,64);
-  app.stage.addChild(player);
+    player.position.set(app.renderer.width/2, app.renderer.height/2);
+    app.stage.addChild(player);
 
-  app.renderer.render();
+    app.renderer.render();
 
 }
